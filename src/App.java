3bf400d223607f435747d22d2782db6736e6d351
ismael2004-oo <alfredoquinchi50@gmail.com>
2025-04-24 22:26:32 -0500

    cambios de juliana

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello,  juliana");
+        System.out.println("esta bien jeje");
     }
 }
