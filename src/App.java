68a4a333c1192e65b046fa22ca56730783dbf2f3
LibretaDeContactos <julianatc12@gmail.com>
2025-04-24 23:52:23 -0500

    nada

--- src/App.java
+++ src/App.java
@@ -1,10 +1,8 @@
+import clases.usuario;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello,  juliana");
-        System.out.println("esta bien jeje");
 
-        for(int i=0; i<=10; i++){
-            System.out.println(i);
-        }
+        
     }
 }
--- src/clases/cliente.java
+++ src/clases/cliente.java
@@ -4,7 +4,7 @@ public class cliente extends classPadre  {
 
     private int id;
 
-    public cliente(String name, String lastname, String age, String email, int id) {
+    public cliente(String name, String lastname, int age, String email, int id) {
         super(name, lastname, age, email);
         this.id = id;
     }
