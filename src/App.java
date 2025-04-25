4fe7e9f00cde1577015b65c23e83d6662ae38f21
LibretaDeContactos <julianatc12@gmail.com>
2025-04-24 22:54:17 -0500

    no se

--- src/App.java
+++ src/App.java
@@ -2,5 +2,9 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello,  juliana");
         System.out.println("esta bien jeje");
+
+        for(int i=0; i<=10; i++){
+            System.out.println(i);
+        }
     }
 }
